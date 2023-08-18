25d1023bc98ba6addfe0cac0e8e78069f88b6f9d
SukhrobQodirov <student.sukhrobqodirov@gmail.com>
2023-08-19 02:38:09 +0500

    half of website is completed

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let options = {
+    strings: [
+        'Sukhrob Qodirov',
+        'front end Developer',
+        '18 years old'
+    ],
+    typeSpeed: 50,
+    startDelay: 500,
+    loop: true,
+    backSpeed: 40,
+    backDelay: 500,
+}
+
+let typed = new Typed('.element', options);
