46b87bc89754321ba2b18e41d276a9f0f5afdc62
SukhrobQodirov <student.sukhrobqodirov@gmail.com>
2023-08-19 03:58:28 +0500

    add some animation

--- js/script.js
+++ js/script.js
@@ -1,14 +1,51 @@
-let options = {
-    strings: [
-        'Sukhrob Qodirov',
-        'front end Developer',
-        '18 years old'
-    ],
-    typeSpeed: 50,
-    startDelay: 500,
-    loop: true,
-    backSpeed: 40,
-    backDelay: 500,
-}
+$(document).ready(function () {
 
-let typed = new Typed('.element', options);
+    let options = {
+        strings: [
+            'Sukhrob Qodirov',
+            'front end Developer',
+            '18 years old'
+        ],
+        typeSpeed: 50,
+        startDelay: 500,
+        loop: true,
+        backSpeed: 40,
+        backDelay: 500,
+    }
+
+    let typed = new Typed('.element', options);
+
+    $('.projects__wrapper').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        cssEase: 'linear',
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+
+})
